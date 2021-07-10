c9f4c2b1d2f18e91fd6b2a6e4a9d68ce0f4f06ad
Janmansh <janmansh.191cs205@nitk.edu.in>
2021-07-10 19:59:01 +0530

    Atcoder ABC #209

---
+++ Atcoder/ABC209/problem4/problem4.cpp
@@ -0,0 +1,175 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define int long long
+#define pb push_back
+#define mod 1000000007
+#define mp make_pair
+
+ll fact[200005];
+vector<vector<int>>g(100001);
+vector<int>vis(100001,0);
+
+ll powermod(ll x,ll y){
+  if(y==0) return 1;
+  ll temp = powermod( x,y/2 )%mod;
+  if( y%2 ){
+    return (((temp*temp)%mod)*x%mod);
+  }
+  return (temp*temp)%mod;
+}
+
+ll power(ll x,ll y){
+  if(y==0) return 1;
+  ll temp = power( x,y/2 );
+  if( y%2 ){
+    return (((temp*temp))*x);
+  }
+  return (temp*temp);
+}
+
+int gcd (int a, int b) {
+    return b ? gcd (b, a % b) : a;
+}
+
+ll inv(ll a, ll p){
+	return powermod(a,mod-2);
+}
+ 
+ll nCr(ll n, ll r, ll p){
+	if(r > n) return 0;
+	ll t1 = fact[n];
+	ll t2 = inv(fact[r],p);
+	ll t3 = inv(fact[n-r],p);
+ 
+	return (((t1*t2)%p)*t3)%p;
+}
+
+
+vector<int> height, euler, first, segtree;
+    vector<bool> visited;
+    int n;
+
+    
+
+    void dfs(vector<vector<int>> &adj, int node, int h = 0) {
+        visited[node] = true;
+        height[node] = h;
+        first[node] = euler.size();
+        euler.push_back(node);
+        for (auto to : adj[node]) {
+            if (!visited[to]) {
+                dfs(adj, to, h + 1);
+                euler.push_back(node);
+            }
+        }
+    }
+
+    void build(int node, int b, int e) {
+        if (b == e) {
+            segtree[node] = euler[b];
+        } else {
+            int mid = (b + e) / 2;
+            build(node << 1, b, mid);
+            build(node << 1 | 1, mid + 1, e);
+            int l = segtree[node << 1], r = segtree[node << 1 | 1];
+            segtree[node] = (height[l] < height[r]) ? l : r;
+        }
+    }
+
+    int query(int node, int b, int e, int L, int R) {
+        if (b > R || e < L)
+            return -1;
+        if (b >= L && e <= R)
+            return segtree[node];
+        int mid = (b + e) >> 1;
+
+        int left = query(node << 1, b, mid, L, R);
+        int right = query(node << 1 | 1, mid + 1, e, L, R);
+        if (left == -1) return right;
+        if (right == -1) return left;
+        return height[left] < height[right] ? left : right;
+    }
+
+    int lca(int u, int v) {
+        int left = first[u], right = first[v];
+        if (left > right)
+            swap(left, right);
+        return query(1, 0, euler.size() - 1, left, right);
+    }
+    
+    vector<int>d(100001, 0);
+    
+    void LCA(vector<vector<int>> &adj, int root = 0) {
+        n = adj.size();
+        height.resize(n);
+        first.resize(n);
+        euler.reserve(n * 2);
+        visited.assign(n, false);
+        dfs(adj, root);
+        int m = euler.size();
+        segtree.resize(m * 4);
+        build(1, 0, m - 1);
+    }
+
+void dijkstra(int root){
+  d[root]=0;
+  vis[root]=1;
+  queue<int>q;
+  q.push(root);
+  while(!q.empty()){
+    int v = q.front();
+    q.pop();
+    vis[v]=1;
+    for(int i=0;i<g[v].size();i++){
+      if(vis[g[v][i]])continue;
+      d[g[v][i]] = d[v]+1;
+      q.push(g[v][i]);
+    }
+  }
+  return;
+}
+
+void solve(){
+  int n,i,j,q;
+  cin>>n>>q;
+  for(i=0;i<n-1;i++){
+    ll u,v;
+    cin>>u>>v;
+    g[u].pb(v);
+    g[v].pb(u);
+  }
+  g[0].pb(1);
+  g[1].pb(0);
+  LCA(g);
+  dijkstra(0);
+  for(i=0;i<q;i++){
+    int u,v;
+    cin>>u>>v;
+    //u--;
+    //v--;
+    int x = lca(u,v);
+    //cout<<x<<" ";
+    int y = d[v]+d[u]-2*d[x];
+    //cout<<d[v]<<" "<<d[u]<<" ";
+    if(y%2)cout<<"Road\n";
+    else cout<<"Town\n";
+  }
+  return;
+}
+
+signed main(){
+  ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+  ll t=1;
+  //cin>>t;
+  //srand(time(0));
+  // fact[0]=1;
+  // for(int i=1;i<200001;i++){
+  //   fact[i]=i*fact[i-1];
+  //   fact[i]%=mod;
+  // }
+  while (t--){
+    solve();
+  }
+  return 0;
+}
